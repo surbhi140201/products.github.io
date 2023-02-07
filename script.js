45dcabd2e07550abb3b330a2a9fbbbc4d5fc03aa
surbhi <surbhi1402001@gmail.com>
2023-02-07 19:49:01 +0530

    my products website

---
+++ script.js
@@ -0,0 +1,18 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+// if (bar) {
+//     bar.addEventListener('click', () => {
+//         nav.classList.add('active');
+//     })
+// }
+
+
+
+function fn(){
+    nav.classList.add('active');
+}
+
+function fn2(){
+    nav.classList.remove('active');
+}
